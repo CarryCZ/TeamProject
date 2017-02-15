d606746b4e1e6e7844273b3d0682cbe8155c02dd
Trafina <ttrafina@gmail.com>
2017-02-15 13:04:15 +0100

    GUI caller added

---
+++ LegoGuitarist/src/gui/GuiExecutor.java
@@ -0,0 +1,21 @@
+package gui;
+
+
+/**
+ * Contains only method {@link #main(String[])} which calls for GUI execution.
+ * 
+ * @author Tomas Trafina - ttrafina.at.gmail.com
+ *
+ */
+public final class GuiExecutor {
+
+	/**
+	 * Should contain only construct for the main GUI class and its startup.
+	 * 
+	 * @param args - input parameters from console execution
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
