22304ad2c20d826e33ee2743e524d06b63960dd0
Trafina <Trafina@Trafina-PC>
2017-02-21 22:08:17 +0100

    Refactored motors handling

--- LegoGuitarist/src/guitar/GuitarMotorsHandler.java
+++ LegoGuitarist/src/guitar/GuitarMotorsHandler.java
@@ -1,9 +1,12 @@
 package guitar;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 
-import lejos.hardware.Brick;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.remote.ev3.RemoteRequestEV3;
 import lejos.robotics.RegulatedMotor;
 
 /**
@@ -15,17 +18,25 @@ import lejos.robotics.RegulatedMotor;
 public class GuitarMotorsHandler {
 	/** logger for program messages output feed */
 	private static final Logger LOGGER = Logger.getLogger(GuitarMotorsHandler.class);
-	
+	/** field of all six motors used in the setup {fret1, fret2, fret3, strumming, rhythm, root} */
 	private final RegulatedMotor[] motors = new RegulatedMotor[6];
 	
-	public GuitarMotorsHandler(Brick master, Brick slave) {
+	public GuitarMotorsHandler(GuitarBrick master, GuitarBrick slave) {
 		LOGGER.debug("Object: GuitarMotorshandler created.");
-		motors[0] = new EV3LargeRegulatedMotor(slave.getPort("A"));
-		motors[1] = new EV3LargeRegulatedMotor(slave.getPort("B"));
-		motors[2] = new EV3LargeRegulatedMotor(slave.getPort("C"));
-		motors[3] = new EV3LargeRegulatedMotor(master.getPort("A"));
-		motors[4] = new EV3LargeRegulatedMotor(master.getPort("B"));
-		motors[5] = new EV3LargeRegulatedMotor(master.getPort("C"));
+		RemoteRequestEV3 remote = null;
+		try {
+			remote = new RemoteRequestEV3(slave.getIpAddress());
+		} catch (IOException e) {
+			LOGGER.error("GuitarMotorsHandler: Error in initiation of remote access.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		motors[0] = remote.createRegulatedMotor(GuitarConstants.MOTOR_PORT_FRET1, GuitarConstants.MOTOR_ID_LARGE);
+		motors[1] = remote.createRegulatedMotor(GuitarConstants.MOTOR_PORT_FRET2, GuitarConstants.MOTOR_ID_LARGE);
+		motors[2] = remote.createRegulatedMotor(GuitarConstants.MOTOR_PORT_FRET3, GuitarConstants.MOTOR_ID_LARGE);
+		motors[3] = new EV3LargeRegulatedMotor(master.getBrick().getPort(GuitarConstants.MOTOR_PORT_STRUMMING));
+		motors[4] = new EV3MediumRegulatedMotor(master.getBrick().getPort(GuitarConstants.MOTOR_PORT_RHYTM_SELECTOR));
+		motors[5] = new EV3LargeRegulatedMotor(master.getBrick().getPort(GuitarConstants.MOTOR_PORT_ROOT_SELECTOR));
 		
 		//TODO: Some motors should be reversed for theirs correct operation.
 		for(int index=0; index<motors.length; index++) {
@@ -44,13 +55,13 @@ public class GuitarMotorsHandler {
 		     public void run() {
 		          switch(port) {
 		          case "A":
-		        	  motors[3].rotate(60);
+		        	  motors[3].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		        	  break;
 		          case "B":
-		        	  motors[4].rotate(60);
+		        	  motors[4].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		        	  break;
 		          case "C":
-		        	  motors[5].rotate(60);
+		        	  motors[5].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		          default:
 		        	  throw new IllegalArgumentException("This method accepts only {\"A\",\"B\" or \"C\"}.");
 		          }
@@ -68,13 +79,13 @@ public class GuitarMotorsHandler {
 		     public void run() {
 		          switch(port) {
 		          case "A":
-		        	  motors[0].rotate(60);
+		        	  motors[0].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		        	  break;
 		          case "B":
-		        	  motors[1].rotate(60);
+		        	  motors[1].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		        	  break;
 		          case "C":
-		        	  motors[2].rotate(60);
+		        	  motors[2].rotate(GuitarConstants.MOTOR_ONE_STEP);
 		          default:
 		        	  throw new IllegalArgumentException("This method accepts only {\"A\",\"B\" or \"C\"}.");
 		          }
