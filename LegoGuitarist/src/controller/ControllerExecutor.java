b450f84f656cb7d132a4190bbca69b2dfe3d808c
Novotny <vaclav.novotny93@gmail.com>
2017-03-22 01:14:22 +0100

    Implemented ControllerExecutor class. Reads the file ChordRecord.bin and by the timeStamp changes the position of the three machines.

--- LegoGuitarist/src/controller/ControllerExecutor.java
+++ LegoGuitarist/src/controller/ControllerExecutor.java
@@ -2,6 +2,8 @@ package controller;
 
 import file.BinaryReader;
 import file.ChordRecord;
+import file.RhythmTempoRecord;
+import guitar.GuitarMotorsHandler;
 
 /**
  * Contains only method {@link #main(String[])} which calls for controller execution.
@@ -10,15 +12,66 @@ import file.ChordRecord;
  *
  */
 public class ControllerExecutor {
-
+	
 	/**
 	 * Should contain only construct for the main controller class and its startup.
 	 * 
 	 * @param args - input parameters from console execution
 	 */
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		
+		//Reading ChordRecord binary file
+		ChordRecord chordRecordPrevious = null;
+		ChordRecord chordRecord = new ChordRecord();
 		
+		BinaryReader binaryReaderForChordRecord = new BinaryReader("inputBinary", chordRecord.getPacketSize());
+		
+		while (binaryReaderForChordRecord.hasNext()) {
+			
+			chordRecordPrevious = chordRecord;
+			chordRecord = new ChordRecord();
+			chordRecord.fillFromBuffer(binaryReaderForChordRecord.next());
+			//System.out.println("Prave jsme precetli akord: " + chordRecord.getChord());
+			int [] arraySteps = chordRecordPrevious.getChord().stepsRequiredFortransitionTo(chordRecord.getChord());
+			
+			GuitarMotorsHandler quitarMotorsHandler = new GuitarMotorsHandler(null, null);
+			
+			long holdTimeChord = chordRecord.getTimeStamp() - chordRecordPrevious.getTimeStamp();
+			
+			try {
+			    Thread.sleep(holdTimeChord);               
+			    } 
+			catch(InterruptedException ex) {
+			Thread.currentThread().interrupt();
+			}
+			
+			for(int i = 0; i < arraySteps[0]; i++) {
+				quitarMotorsHandler.makeStepFret(1);
+			}
+			
+			for(int i = 0; i < arraySteps[1]; i++) {
+				quitarMotorsHandler.makeStepFret(2);
+			}
+			
+			for(int i = 0; i < arraySteps[2]; i++){
+				quitarMotorsHandler.makeStepFret(3);
+			}
+			
+		}
+		binaryReaderForChordRecord.close();
+
+
+		/*
+		//Reading RhythmTempoRecord binary file
+		RhythmTempoRecord rhythmTempoRecordEmpty = new RhythmTempoRecord();
+		BinaryReader binaryReaderForRhythmTempoRecord = new BinaryReader("inputBinary", rhythmTempoRecordEmpty.getPacketSize());
+		while (binaryReaderForRhythmTempoRecord.hasNext()) {
+			rhythmTempoRecordEmpty.fillFromBuffer(binaryReaderForRhythmTempoRecord.next());
+		}
+		binaryReaderForRhythmTempoRecord.close();
+		*/
+
+				
 	}
 
 }
